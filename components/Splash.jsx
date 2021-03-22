ac6f89785b7a63ebdda67c7b710435e5b516fcc7
ndsanja <ndsanja@gmail.com>
2021-03-23 00:08:34 +0700

    init commit

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import Link from "next/link";
 
 export default function Splash() {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-full bg-gradient-to-br from-green-200 to-green-100">
-      <div className="flex space-x-2">
-        <div className="font-extrabold text-9xl font-mono tracking-tight">
-          LURU
-        </div>
-        <div className="h-6 w-6 rounded-full bg-black"></div>
-      </div>
+      <Link href="/home">
+        <a>
+          <div className="flex space-x-2">
+            <div className="font-extrabold text-9xl font-mono tracking-tight">
+              LURU
+            </div>
+            <div className="h-6 w-6 rounded-full bg-black"></div>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 }
